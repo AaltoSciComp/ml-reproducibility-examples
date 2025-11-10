4d486a954b96baf864ad044649e7ced4f6617b06
Simo Tuomisto <simo.tuomisto@aalto.fi>
2025-11-10 13:38:59 +0200

    modular_training: Fixed some bugs with logging

--- modular_training/examples/example3/train_mnist_lightning.py
+++ modular_training/examples/example3/train_mnist_lightning.py
@@ -13,9 +13,12 @@ import pandas as pd
 
 import lightning as L
 from lightning.pytorch.callbacks import TQDMProgressBar
+from lightning.pytorch.loggers import CSVLogger
 
 from datamodules import MNISTDataModule
 from models import SimpleMLP
+
+import os.path
 
 
 def plot_training(metrics):
@@ -38,20 +41,29 @@ def main():
     hidden_size = 20
 
     training_plot = "training_results.png"
+    log_dir = "logs"
+    experiment_name = "mnist_example"
 
     # Create model and datamodule
     model = SimpleMLP(hidden_size=hidden_size)
     datamodule = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
 
+    # Specify logger
+
+    logger = CSVLogger(log_dir, name=experiment_name)
+
     # Train the model
     trainer = L.Trainer(
         max_epochs=5,
         callbacks=[TQDMProgressBar(refresh_rate=100)],
+        logger=logger,
     )
     trainer.fit(model, datamodule=datamodule)
 
     # Visualize training
-    metrics = pd.read_csv("lightning_logs/version_0/metrics.csv")
+    metrics = pd.read_csv(
+        os.path.join(log_dir, experiment_name, "version_0", "metrics.csv")
+    )
     fig, axes = plot_training(metrics)
     fig.savefig(training_plot)
 
