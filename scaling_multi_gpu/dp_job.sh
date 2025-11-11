dc984162181962e3126ad08302710040734e3e70
Hossein Firooz <hsnfirooz@gmail.com>
2025-11-11 09:00:42 +0200

    Create dp_job.sh

---
+++ scaling_multi_gpu/dp_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=ddp_cifar100
+#SBATCH --partition=gpu-h200-141g-ellis
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-node=2
+#SBATCH --mem=60G
+#SBATCH --time=0:30:00
+#SBATCH --output=ddp_cifar100-%j.out
+
+module load scicomp-python-env
+
+python dp.py
