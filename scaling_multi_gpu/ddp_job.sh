9d843ad2b697e0a53ab75d30dfff8db0798fb8d0
Hossein Firooz <hsnfirooz@gmail.com>
2025-11-11 08:58:05 +0200

    Create ddp_job.sh

---
+++ scaling_multi_gpu/ddp_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=ddp_cifar100
+#SBATCH --partition=gpu-h200-141g-ellis
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gpus-per-node=2
+#SBATCH --mem=60G
+#SBATCH --time=0:30:00
+#SBATCH --output=ddp_cifar100-%j.out
+
+module load scicomp-python-env
+
+OMP_NUM_THREADS=1 python -m torch.distributed.run --standalone --nnodes=1 --nproc_per_node=2 ddp.py
